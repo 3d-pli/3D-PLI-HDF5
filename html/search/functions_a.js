488b8adaca26be92433143892af26a6c9397fbb3
Thyre <Thyre@users.noreply.github.com>
2022-05-04 09:15:46 +0000

    deploy: 21791450d7ca1dd7b0e03edabd04eeb308cfe3da

--- html/search/functions_a.js
+++ html/search/functions_a.js
@@ -1,9 +1,10 @@
 var searchData=
 [
-  ['open_0',['open',['../classPLI_1_1HDF5_1_1Dataset.html#ac2da3c02f06d57236a1eecd0f11baf47',1,'PLI::HDF5::Dataset::open()'],['../classPLI_1_1HDF5_1_1File.html#a9d30180bacdbb792b5d72b4ace1ed07c',1,'PLI::HDF5::File::open()'],['../classPLI_1_1HDF5_1_1Group.html#aed1852d153f5c8ab32392b99e3d5d8e4',1,'PLI::HDF5::Group::open()']]],
+  ['open_0',['open',['../classPLI_1_1HDF5_1_1Dataset.html#ac2da3c02f06d57236a1eecd0f11baf47',1,'PLI::HDF5::Dataset::open()'],['../classPLI_1_1HDF5_1_1File.html#a9d30180bacdbb792b5d72b4ace1ed07c',1,'PLI::HDF5::File::open()'],['../classPLI_1_1HDF5_1_1Group.html#a4cf4b28419196b2bbff1881203eac780',1,'PLI::HDF5::Group::open()']]],
   ['opendataset_1',['openDataset',['../namespacePLI_1_1HDF5.html#ab40573c6538568ac633f7e87f8fa9159',1,'PLI::HDF5']]],
   ['openfile_2',['openFile',['../namespacePLI_1_1HDF5.html#a793947fa88ab4a9dbb2931da321dfba5',1,'PLI::HDF5']]],
-  ['operator_20hid_5ft_3',['operator hid_t',['../classPLI_1_1HDF5_1_1Dataset.html#ae471843cd9ab5999bf0b407988905d81',1,'PLI::HDF5::Dataset::operator hid_t()'],['../classPLI_1_1HDF5_1_1File.html#a7c53c2e016fd0243cf5f4162e43637d2',1,'PLI::HDF5::File::operator hid_t()'],['../classPLI_1_1HDF5_1_1Group.html#a1331843612104a553c4b03f5825b5dcd',1,'PLI::HDF5::Group::operator hid_t()'],['../classPLI_1_1HDF5_1_1Type.html#a55f790fde4567ad2a45bee1e26cb387f',1,'PLI::HDF5::Type::operator hid_t() const']]],
-  ['operator_3d_3d_4',['operator==',['../classPLI_1_1HDF5_1_1Type.html#a8555755d59e9eb8371674428c44f2855',1,'PLI::HDF5::Type']]],
-  ['string_5',['string',['../classPLI_1_1HDF5_1_1Type.html#a39535681908f4b9185128451d3c6e3db',1,'PLI::HDF5::Type']]]
+  ['opengroup_3',['openGroup',['../namespacePLI_1_1HDF5.html#a906ea863179d6d9e70ad870ae8493b1c',1,'PLI::HDF5']]],
+  ['operator_20hid_5ft_4',['operator hid_t',['../classPLI_1_1HDF5_1_1Dataset.html#ae471843cd9ab5999bf0b407988905d81',1,'PLI::HDF5::Dataset::operator hid_t()'],['../classPLI_1_1HDF5_1_1File.html#a7c53c2e016fd0243cf5f4162e43637d2',1,'PLI::HDF5::File::operator hid_t()'],['../classPLI_1_1HDF5_1_1Group.html#a1331843612104a553c4b03f5825b5dcd',1,'PLI::HDF5::Group::operator hid_t()'],['../classPLI_1_1HDF5_1_1Type.html#a55f790fde4567ad2a45bee1e26cb387f',1,'PLI::HDF5::Type::operator hid_t() const']]],
+  ['operator_3d_3d_5',['operator==',['../classPLI_1_1HDF5_1_1Type.html#a8555755d59e9eb8371674428c44f2855',1,'PLI::HDF5::Type']]],
+  ['string_6',['string',['../classPLI_1_1HDF5_1_1Type.html#a39535681908f4b9185128451d3c6e3db',1,'PLI::HDF5::Type']]]
 ];
