340241ba0dece26c859ac44843001bf83e33ed1e
fmatuschke <f.matuschke@fz-juelich.de>
2022-05-04 11:26:59 +0200

    FIX applied clang-format

--- test/test_file.cpp
+++ test/test_file.cpp
@@ -28,11 +28,8 @@
 #include "PLIHDF5/file.h"
 #include "testEnvironment.h"
 
-TEST(PLI_HDF5_File, Constructor)
-{
-  EXPECT_NO_THROW({
-    auto h5f = PLI::HDF5::File();
-  });
+TEST(PLI_HDF5_File, Constructor) {
+  EXPECT_NO_THROW({ auto h5f = PLI::HDF5::File(); });
 
   EXPECT_NO_THROW({
     auto h5f_0 = PLI::HDF5::File();
@@ -42,42 +39,39 @@ TEST(PLI_HDF5_File, Constructor)
   // TODO: File(const hid_t filePtr, const hid_t faplID);
 }
 
-TEST(PLI_HDF5_File, Destructor)
-{
-  EXPECT_NO_THROW({ // invoke deconstroctor
+TEST(PLI_HDF5_File, Destructor) {
+  EXPECT_NO_THROW({  // invoke deconstroctor
     auto h5f = PLI::HDF5::File();
   });
 }
 
-TEST(PLI_HDF5_File, Create)
-{
-  { // create file
+TEST(PLI_HDF5_File, Create) {
+  {  // create file
     auto h5f = PLI::HDF5::File();
     h5f.create(kTmpFile);
   }
 
-  { // create existing file
+  {  // create existing file
     auto h5f = PLI::HDF5::File();
-    EXPECT_THROW(h5f.create(kTmpFile), PLI::HDF5::Exceptions::FileExistsException);
+    EXPECT_THROW(h5f.create(kTmpFile),
+                 PLI::HDF5::Exceptions::FileExistsException);
   }
 }
 
-TEST(PLI_HDF5_File, Open)
-{
-  { // create dummy file
+TEST(PLI_HDF5_File, Open) {
+  {  // create dummy file
     auto h5f = PLI::HDF5::File();
     h5f.create(kTmpFile);
   }
 
-  { // open dummy file
+  {  // open dummy file
     auto h5f = PLI::HDF5::File();
     EXPECT_TRUE(h5f.open(kTmpFile, PLI::HDF5::File::OpenState::ReadWrite));
   }
 }
 
-TEST(PLI_HDF5_File, IsHDF5)
-{
-  { // create dummy file
+TEST(PLI_HDF5_File, IsHDF5) {
+  {  // create dummy file
     auto h5f = PLI::HDF5::File();
     h5f.create(kTmpFile);
   }
@@ -85,17 +79,15 @@ TEST(PLI_HDF5_File, IsHDF5)
   ASSERT_TRUE(PLI::HDF5::File::isHDF5(kTmpFile));
 }
 
-TEST(PLI_HDF5_File, FileExists)
-{
-  { // create dummy file
+TEST(PLI_HDF5_File, FileExists) {
+  {  // create dummy file
     auto h5f = PLI::HDF5::File();
     h5f.create(kTmpFile);
   }
   EXPECT_TRUE(PLI::HDF5::File::fileExists(kTmpFile));
 }
 
-TEST(PLI_HDF5_File, Close)
-{
+TEST(PLI_HDF5_File, Close) {
   EXPECT_NO_THROW({
     auto h5f = PLI::HDF5::File();
     h5f.create(kTmpFile);
@@ -103,14 +95,13 @@ TEST(PLI_HDF5_File, Close)
   });
 }
 
-TEST(PLI_HDF5_File, Reopen)
-{
-  { // reopen non set file object
+TEST(PLI_HDF5_File, Reopen) {
+  {  // reopen non set file object
     auto h5f = PLI::HDF5::File();
     EXPECT_ANY_THROW(h5f.reopen());
   }
 
-  { // reopen closes file object
+  {  // reopen closes file object
     auto h5f = PLI::HDF5::File();
     h5f.create(kTmpFile);
     h5f.close();
@@ -118,47 +109,44 @@ TEST(PLI_HDF5_File, Reopen)
     std::filesystem::remove(kTmpFile);
   }
 
-  { // reopen removed file
+  {  // reopen removed file
     auto h5f = PLI::HDF5::File();
     h5f.create(kTmpFile);
     h5f.close();
     std::filesystem::remove(kTmpFile);
-    EXPECT_ANY_THROW(h5f.reopen()); // ???: InvalidHDF5FileException or HDF5RuntimeException
+    EXPECT_ANY_THROW(
+        h5f.reopen());  // ???: InvalidHDF5FileException or HDF5RuntimeException
   }
 }
 
-TEST(PLI_HDF5_File, Flush)
-{
-  EXPECT_ANY_THROW({ // flush non created file
+TEST(PLI_HDF5_File, Flush) {
+  EXPECT_ANY_THROW({  // flush non created file
     auto h5f = PLI::HDF5::File();
     h5f.flush();
     // TODO: specify throw
   });
   std::filesystem::remove(kTmpFile);
 
-  EXPECT_NO_THROW({ // flush empty file
+  EXPECT_NO_THROW({  // flush empty file
     auto h5f = PLI::HDF5::File();
     h5f.create(kTmpFile);
     h5f.flush();
   });
 }
 
-TEST(PLI_HDF5_File, ID)
-{
+TEST(PLI_HDF5_File, ID) {
   auto h5f = PLI::HDF5::File();
   h5f.create(kTmpFile);
   ASSERT_TRUE(h5f.id() >= 0);
 }
 
-TEST(PLI_HDF5_File, FaplID)
-{
+TEST(PLI_HDF5_File, FaplID) {
   auto h5f = PLI::HDF5::File();
   h5f.create(kTmpFile);
   ASSERT_TRUE(h5f.faplID() >= 0);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   int result = 0;
 
   ::testing::InitGoogleTest(&argc, argv);
