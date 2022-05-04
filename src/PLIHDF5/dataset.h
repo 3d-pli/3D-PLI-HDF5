ec8dcf4ad9d8a795fb396796db8d29c54cb5bb9f
Jan André Reuter <jan.andre.reuter@hotmail.de>
2022-05-04 11:52:22 +0200

    Further doc typo fixes

--- src/PLIHDF5/dataset.h
+++ src/PLIHDF5/dataset.h
@@ -196,25 +196,25 @@ class Dataset {
   /**
    * @brief Get the type of the dataset.
    * @return const PLI::HDF5::Type Type of the dataset
-   * @throws PLI::HDF5::Exceptions:IdentifierNotValidException If the dataset is
-   * not valid or the type doesn't exist.
+   * @throws PLI::HDF5::Exceptions::IdentifierNotValidException If the dataset
+   * is not valid or the type doesn't exist.
    */
   const PLI::HDF5::Type type() const;
   /**
    * @brief Returns the number of dimensions of the dataset.
    * @return int The number of dimensions of the dataset.
-   * @throws PLI::HDF5::Exceptions:IdentifierNotValidException If the dataset is
-   * not valid.
+   * @throws PLI::HDF5::Exceptions::IdentifierNotValidException If the dataset
+   * is not valid.
    */
   int ndims() const;
   /**
    * @brief Returns the number of elements of the dataset.
    * @return const std::vector<hsize_t> The number of elements of the dataset in
    * each dimension.
-   * @throws PLI::HDF5::Exceptions:IdentifierNotValidException If the dataset is
-   * not valid.
-   * @throws PLI::HDF5::Exceptions:HDF5RuntimeException Error during closing the
-   * dataspace of the dataset.
+   * @throws PLI::HDF5::Exceptions::IdentifierNotValidException If the dataset
+   * is not valid.
+   * @throws PLI::HDF5::Exceptions::HDF5RuntimeException Error during closing
+   * the dataspace of the dataset.
    */
   const std::vector<hsize_t> dims() const;
   /**
