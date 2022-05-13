104af477b2fb5729b0c33008729c4c12a91859c9
Jan André Reuter <jan.andre.reuter@hotmail.de>
2022-05-13 23:42:42 +0200

    Finding the package via CMake (only on make install)

--- cmake/package.cmake
+++ cmake/package.cmake
@@ -1,5 +1,38 @@
 find_program( LSB_RELEASE_EXEC lsb_release )
 
+##########
+# Generate CMake files for install
+##########
+export(TARGETS PLIHDF5 FILE ${PROJECT_BINARY_DIR}/PLIHDF5Targets.cmake)
+export(PACKAGE PLIHDF5)
+# Create the PLIHDF5Config.cmake and PLIHDF5ConfigVersion files
+file(RELATIVE_PATH REL_INCLUDE_DIR "${INSTALL_CMAKE_DIR}"
+   "${INSTALL_INCLUDE_DIR}")
+# ... for the build tree
+set(CONF_INCLUDE_DIRS "${PROJECT_SOURCE_DIR}" "${PROJECT_BINARY_DIR}")
+configure_file(cmake/PLIHDF5Config.cmake.in
+  "${PROJECT_BINARY_DIR}/PLIHDF5Config.cmake" @ONLY)
+# ... for the install tree
+set(CONF_INCLUDE_DIRS "\${PLIHDF5_CMAKE_DIR}/${REL_INCLUDE_DIR}")
+configure_file(cmake/PLIHDF5Config.cmake.in
+  "${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/PLIHDF5Config.cmake" @ONLY)
+# ... for both
+configure_file(cmake/PLIHDF5ConfigVersion.cmake.in
+  "${PROJECT_BINARY_DIR}/PLIHDF5ConfigVersion.cmake" @ONLY)
+
+# Install the PLIHDF5Config.cmake and FooBarConfigVersion.cmake
+install(FILES
+  "${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/PLIHDF5Config.cmake"
+  "${PROJECT_BINARY_DIR}/PLIHDF5ConfigVersion.cmake"
+  DESTINATION "${INSTALL_CMAKE_DIR}" COMPONENT dev)
+
+# Install the export set for use with the install-tree
+install(EXPORT PLIHDF5Targets DESTINATION
+"${INSTALL_CMAKE_DIR}" COMPONENT dev)
+
+##########
+# Create packages
+##########
 if( LSB_RELEASE_EXEC )
   # Get the OS release
   execute_process( COMMAND ${LSB_RELEASE_EXEC} -is 
