715d1b1a07dcce76a66c2a1b5f70918b426a4e23
Reuter, Jan Andre <j.reuter@fz-juelich.de>
2022-07-15 17:00:57 +0200

    Add test step to pipeline

--- test/test_file.cpp
+++ test/test_file.cpp
@@ -27,160 +27,167 @@
 
 #include "PLIHDF5/file.h"
 
-void removeFile(const std::string& path) {
-  int32_t rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  if (rank == 0 && std::filesystem::exists(path)) std::filesystem::remove(path);
-  MPI_Barrier(MPI_COMM_WORLD);
+void removeFile(const std::string &path) {
+    int32_t rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 0 && std::filesystem::exists(path))
+        std::filesystem::remove(path);
+    MPI_Barrier(MPI_COMM_WORLD);
 }
 
 class PLI_HDF5_File : public ::testing::Test {
- protected:
-  void SetUp() override {}
+  protected:
+    void SetUp() override {}
 
-  void TearDown() override { removeFile(_filePath); }
+    void TearDown() override { removeFile(_filePath); }
 
-  const std::string _filePath =
-      std::filesystem::temp_directory_path() / "test_file.h5";
+    const std::string _filePath =
+        std::filesystem::temp_directory_path() / "test_file.h5";
 };
 
 TEST_F(PLI_HDF5_File, Constructor) {
-  EXPECT_NO_THROW(auto h5f = PLI::HDF5::File());
+    EXPECT_NO_THROW(auto h5f = PLI::HDF5::File());
 
-  EXPECT_NO_THROW({
-    auto h5f_0 = PLI::HDF5::File();
-    auto h5f_1 = PLI::HDF5::File(h5f_0);
-  });
+    EXPECT_NO_THROW({
+        auto h5f_0 = PLI::HDF5::File();
+        auto h5f_1 = PLI::HDF5::File(h5f_0);
+    });
 
-  EXPECT_NO_THROW({
-    auto h5f_0 = PLI::HDF5::File();
-    auto h5f_1 = PLI::HDF5::File(h5f_0.id(), h5f_0.faplID());
-  });
+    EXPECT_NO_THROW({
+        auto h5f_0 = PLI::HDF5::File();
+        auto h5f_1 = PLI::HDF5::File(h5f_0.id(), h5f_0.faplID());
+    });
 }
 
 TEST_F(PLI_HDF5_File, Destructor) {
-  EXPECT_NO_THROW({  // invoke deconstroctor
-    auto h5f = PLI::HDF5::File();
-  });
+    EXPECT_NO_THROW({ // invoke deconstroctor
+        auto h5f = PLI::HDF5::File();
+    });
 }
 
 TEST_F(PLI_HDF5_File, Create) {
-  {  // create file
-    auto h5f = PLI::HDF5::File();
-    h5f.create(_filePath);
-    h5f.close();
-  }
+    { // create file
+        auto h5f = PLI::HDF5::File();
+        h5f.create(_filePath);
+        h5f.close();
+    }
 
-  {  // create existing file
-    auto h5f = PLI::HDF5::File();
-    EXPECT_THROW(h5f.create(_filePath),
-                 PLI::HDF5::Exceptions::FileExistsException);
-  }
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    { // create existing file
+        auto h5f = PLI::HDF5::File();
+        EXPECT_THROW(h5f.create(_filePath),
+                     PLI::HDF5::Exceptions::FileExistsException);
+    }
 }
 
 TEST_F(PLI_HDF5_File, Open) {
-  {  // create dummy file
-    auto h5f = PLI::HDF5::File();
-    h5f.create(_filePath);
-    h5f.close();
-  }
+    { // create dummy file
+        auto h5f = PLI::HDF5::File();
+        h5f.create(_filePath);
+        h5f.close();
+    }
 
-  {  // open dummy file
-    auto h5f = PLI::HDF5::File();
-    EXPECT_NO_THROW(h5f.open(_filePath, PLI::HDF5::File::OpenState::ReadWrite));
-  }
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    { // open dummy file
+        auto h5f = PLI::HDF5::File();
+        EXPECT_NO_THROW(
+            h5f.open(_filePath, PLI::HDF5::File::OpenState::ReadWrite));
+    }
 }
 
 TEST_F(PLI_HDF5_File, IsHDF5) {
-  {  // create dummy file
-    auto h5f = PLI::HDF5::File();
-    h5f.create(_filePath);
-    h5f.close();
-  }
+    { // create dummy file
+        auto h5f = PLI::HDF5::File();
+        h5f.create(_filePath);
+        h5f.close();
+    }
 
-  ASSERT_TRUE(PLI::HDF5::File::isHDF5(_filePath));
+    ASSERT_TRUE(PLI::HDF5::File::isHDF5(_filePath));
 }
 
 TEST_F(PLI_HDF5_File, FileExists) {
-  {  // create dummy file
-    auto h5f = PLI::HDF5::File();
-    h5f.create(_filePath);
-  }
-  EXPECT_TRUE(PLI::HDF5::File::fileExists(_filePath));
+    { // create dummy file
+        auto h5f = PLI::HDF5::File();
+        h5f.create(_filePath);
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+    EXPECT_TRUE(PLI::HDF5::File::fileExists(_filePath));
 }
 
 TEST_F(PLI_HDF5_File, Close) {
-  EXPECT_NO_THROW({
-    auto h5f = PLI::HDF5::File();
-    h5f.create(_filePath);
-    h5f.close();
-  });
+    EXPECT_NO_THROW({
+        auto h5f = PLI::HDF5::File();
+        h5f.create(_filePath);
+        h5f.close();
+    });
 }
 
 TEST_F(PLI_HDF5_File, Reopen) {
-  {  // reopen none set file object
-    auto h5f = PLI::HDF5::File();
-    EXPECT_ANY_THROW(h5f.reopen());
-    h5f.close();
-  }
+    { // reopen none set file object
+        auto h5f = PLI::HDF5::File();
+        EXPECT_ANY_THROW(h5f.reopen());
+        h5f.close();
+    }
 
-  EXPECT_NO_THROW({  // reopen closed file object
-    auto h5f = PLI::HDF5::File();
-    h5f.create(_filePath);
-    h5f.flush();
-    h5f.reopen();
-    h5f.close();
-  });
-  removeFile(_filePath);
+    EXPECT_NO_THROW({ // reopen closed file object
+        auto h5f = PLI::HDF5::File();
+        h5f.create(_filePath);
+        h5f.flush();
+        h5f.reopen();
+        h5f.close();
+    });
+    removeFile(_filePath);
 
-  {  // reopen closed file
-    auto h5f = PLI::HDF5::File();
-    h5f.create(_filePath);
-    h5f.close();
-    EXPECT_THROW(h5f.reopen(),
-                 PLI::HDF5::Exceptions::IdentifierNotValidException);
-  }
+    { // reopen closed file
+        auto h5f = PLI::HDF5::File();
+        h5f.create(_filePath);
+        h5f.close();
+        EXPECT_THROW(h5f.reopen(),
+                     PLI::HDF5::Exceptions::IdentifierNotValidException);
+    }
 }
 
 TEST_F(PLI_HDF5_File, Flush) {
-  EXPECT_THROW(
-      {  // flush non created file
+    EXPECT_THROW(
+        { // flush non created file
+            auto h5f = PLI::HDF5::File();
+            h5f.flush();
+            h5f.close();
+        },
+        PLI::HDF5::Exceptions::IdentifierNotValidException);
+    removeFile(_filePath);
+
+    EXPECT_NO_THROW({ // flush empty file
         auto h5f = PLI::HDF5::File();
+        h5f.create(_filePath);
         h5f.flush();
         h5f.close();
-      },
-      PLI::HDF5::Exceptions::IdentifierNotValidException);
-  removeFile(_filePath);
-
-  EXPECT_NO_THROW({  // flush empty file
-    auto h5f = PLI::HDF5::File();
-    h5f.create(_filePath);
-    h5f.flush();
-    h5f.close();
-  });
+    });
 }
 
 TEST_F(PLI_HDF5_File, ID) {
-  auto h5f = PLI::HDF5::File();
-  h5f.create(_filePath);
-  ASSERT_GE(h5f.id(), 0);
-  h5f.close();
+    auto h5f = PLI::HDF5::File();
+    h5f.create(_filePath);
+    ASSERT_GE(h5f.id(), 0);
+    h5f.close();
 }
 
 TEST_F(PLI_HDF5_File, FaplID) {
-  auto h5f = PLI::HDF5::File();
-  h5f.create(_filePath);
-  ASSERT_GE(h5f.faplID(), 0);
-  h5f.close();
+    auto h5f = PLI::HDF5::File();
+    h5f.create(_filePath);
+    ASSERT_GE(h5f.faplID(), 0);
+    h5f.close();
 }
 
-int main(int argc, char* argv[]) {
-  int result = 0;
+int main(int argc, char *argv[]) {
+    int result = 0;
 
-  MPI_Init(&argc, &argv);
-  ::testing::InitGoogleTest(&argc, argv);
-  result = RUN_ALL_TESTS();
+    MPI_Init(&argc, &argv);
+    ::testing::InitGoogleTest(&argc, argv);
+    result = RUN_ALL_TESTS();
 
-  MPI_Finalize();
-  return result;
+    MPI_Finalize();
+    return result;
 }
