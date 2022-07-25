7d841f8b5c8669e331b3e53a81b46e9be955640d
Jan André Reuter <j.reuter@fz-juelich.de>
2022-07-25 15:55:53 +0200

    Update documentation

--- src/PLIHDF5/object.h
+++ src/PLIHDF5/object.h
@@ -38,20 +38,53 @@ namespace PLI::HDF5 {
 class Group;
 class Dataset;
 
+/**
+ * @brief HDF5 Object class
+ * Each HDF5 file contains groups, datasets and attributes. Each one can be
+ * described through an HDF5 Object (H5O). This class offers a small wrapper
+ * to help with other HDF5 functionality.
+ */
 class Object {
   public:
-    explicit Object(const std::optional<MPI_Comm> &communicator = {}) noexcept;
-    explicit Object(const hid_t id,
-                    const std::optional<MPI_Comm> &communicator = {}) noexcept;
     ~Object();
 
+    /**
+     * @brief Get the raw HDF5 pointer of the object.
+     *
+     * Returns the object HDF5 pointer of the object. This pointer can be used
+     * to access the dataset using the HDF5 library.
+     * @return hid_t Object ID stored.
+     */
     hid_t id() const noexcept;
+    /**
+     * @brief Returns the MPI_Communicator set when creating an HDF5 object.
+     * Certain HDF5 objects like File and Dataset might need an MPI_Comm to
+     * define their transaction. To help with this behaviour, the communicator
+     * used for all operations is stored in the Object class and passed to all
+     * other created instances.
+     * @return MPI_Comm if one is set, else an empty optional.
+     */
     std::optional<MPI_Comm> communicator() const noexcept;
+    /**
+     * @brief Convert the dataset to a the raw HDF5 pointer.
+     * @return hid_t Dataset ID stored in the object.
+     */
     operator hid_t() const noexcept;
-
+    /**
+     * @brief Closes the HDF5 object.
+     * Manually closes the HDF5 object. If a file is closed, all other objects
+     * in that file are closed as well to prevent issues. Calling the method
+     * from a closed instance will do nothing. When closing an HDF5 object which
+     * was copied earlier, the object will still be alive until all instances or
+     * the file are closed.
+     */
     void close();
 
   protected:
+    explicit Object(const std::optional<MPI_Comm> &communicator = {}) noexcept;
+    explicit Object(const hid_t id,
+                    const std::optional<MPI_Comm> &communicator = {}) noexcept;
+
     std::optional<MPI_Comm> m_communicator;
     hid_t m_id;
 
@@ -60,14 +93,10 @@ class Object {
 
 class Folder : public Object {
   public:
-    explicit Folder(const std::optional<MPI_Comm> &communicator = {}) noexcept;
-    explicit Folder(const hid_t id,
-                    const std::optional<MPI_Comm> &communicator = {}) noexcept;
     /**
      * @brief Opens a group
      * Open an existing group. If the group does not exist, an exception is
      * thrown.
-     * @param parentPtr Pointer to the parent group or file.
      * @param groupName Name of the group to open.
      * @return PLI::HDF5::Group Group object, if successful.
      * @throws PLI::HDF5::Exceptions::GroupNotFoundException If the group does
@@ -79,7 +108,6 @@ class Folder : public Object {
     /**
      * @brief Creates a group
      * Create a new group. If the group already exists, an exception is thrown.
-     * @param parentPtr Pointer to the parent group or file.
      * @param groupName Name of the group to create.
      * @return PLI::HDF5::Group Group object, if successful.
      * @throws PLI::HDF5::Exceptions::GroupExistsException If the group already
@@ -94,7 +122,6 @@ class Folder : public Object {
      *
      * This method tries to open an existing dataset with the given name. If the
      * dataset does not exist, an exception is thrown.
-     * @param parentPtr File or group pointer.
      * @param datasetName Name of the dataset to open.
      * @return PLI::HDF5::Dataset Dataset if successful.
      * @throws PLI::HDF5::Exceptions::IdentifierNotValidException If the parent
@@ -113,7 +140,6 @@ class Folder : public Object {
      * @tparam T Supported data types are: bool, char, unsigned char, short,
      * unsigned short, int, unsigned int, long, unsigned long, long long,
      * unsigned long long, float, double, long double.
-     * @param parentPtr Raw HDF5 pointer to a file or group.
      * @param datasetName Name of the dataset to create.
      * @param dims Dimensions of the new dataset.
      * @param chunkDims Chunking dimensions of the dataset. If not set, the
@@ -138,7 +164,6 @@ class Folder : public Object {
      * dataset already exists, an exception is thrown.
      * The dimensions of the dataset need to be set during the method call.
      * Optionally, the chunk dimensions can be set.
-     * @param parentPtr Raw HDF5 pointer to a file or group.
      * @param datasetName Name of the dataset to create.
      * @param dims Dimensions of the new dataset.
      * @param chunkDims Chunking dimensions of the dataset. If not set, the
@@ -156,6 +181,11 @@ class Folder : public Object {
         const std::string &datasetName, const std::vector<size_t> &dims,
         const std::vector<size_t> &chunkDims = {},
         const PLI::HDF5::Type &dataType = PLI::HDF5::Type::createType<float>());
+
+  protected:
+    explicit Folder(const std::optional<MPI_Comm> &communicator = {}) noexcept;
+    explicit Folder(const hid_t id,
+                    const std::optional<MPI_Comm> &communicator = {}) noexcept;
 };
 
 } // namespace PLI::HDF5
