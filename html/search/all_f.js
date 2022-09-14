15b2f75e719371022e8403f2f2b65f8e678b453c
Thyre <Thyre@users.noreply.github.com>
2022-09-14 12:57:36 +0000

    deploy: 6be181052cb42892876bba2249473d4e98a5437f

--- html/search/all_f.js
+++ html/search/all_f.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['read_0',['read',['../classPLI_1_1HDF5_1_1Dataset.html#a9ac175e623638369397e7855c6e8a98c',1,'PLI::HDF5::Dataset']]],
+  ['read_0',['read',['../classPLI_1_1HDF5_1_1Dataset.html#a9ac175e623638369397e7855c6e8a98c',1,'PLI::HDF5::Dataset::read(const std::vector&lt; size_t &gt; &amp;offset, const std::vector&lt; size_t &gt; &amp;count, const std::vector&lt; size_t &gt; &amp;stride={}) const'],['../classPLI_1_1HDF5_1_1Dataset.html#a3fc9b9a2d811319eaa672879d5eaef5f',1,'PLI::HDF5::Dataset::read(T *const data, const std::vector&lt; size_t &gt; &amp;offset, const std::vector&lt; size_t &gt; &amp;count, const std::vector&lt; size_t &gt; &amp;stride={}) const'],['../classPLI_1_1HDF5_1_1Dataset.html#a480e3a8f28aa23c726f1c9b09ebf38d4',1,'PLI::HDF5::Dataset::read(const Hyperslab &amp;hyperslab) const'],['../classPLI_1_1HDF5_1_1Dataset.html#aa9a6b186f3e568973a7fe5303bfc132f',1,'PLI::HDF5::Dataset::read(T *const data, const Hyperslab &amp;hyperslab) const']]],
   ['readfulldataset_1',['readFullDataset',['../classPLI_1_1HDF5_1_1Dataset.html#ac879f9258f2fa5f2d89faf5e6a477105',1,'PLI::HDF5::Dataset']]],
   ['readonly_2',['ReadOnly',['../classPLI_1_1HDF5_1_1File.html#ad41a62cae2d952fe4648fd73585e85c5a131fb182a881796e7606ed6da27f1197',1,'PLI::HDF5::File']]],
   ['readwrite_3',['ReadWrite',['../classPLI_1_1HDF5_1_1File.html#ad41a62cae2d952fe4648fd73585e85c5a70a2a84088d405a2e3f1e3accaa16723',1,'PLI::HDF5::File']]],
